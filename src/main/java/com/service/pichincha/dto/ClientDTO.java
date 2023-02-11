a8514c7acb6de49f7a1d271f718571ea640077ad
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-02-11 18:14:54 -0500

    ajuste dto

--- src/main/java/com/service/pichincha/dto/ClientDTO.java
+++ src/main/java/com/service/pichincha/dto/ClientDTO.java
@@ -2,17 +2,14 @@ package com.service.pichincha.dto;
 
 import com.service.pichincha.entities.enums.GenderPerson;
 import com.service.pichincha.entities.enums.IdentificationPattern;
-import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import java.util.UUID;
 
 @Data
-@Builder
 public class ClientDTO {
-    private UUID clientId;
+    private Long clientId;
     @NotNull
     private String fullName;
     @NotNull
