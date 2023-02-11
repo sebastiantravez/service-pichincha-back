e8e1a3a0156bf188434749e018019529fd63105c
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-02-11 12:23:02 -0500

    big bang

---
+++ src/test/java/com/service/pichincha/PichinchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.pichincha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PichinchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
